fedaaad0c48b03876da02811f5398ea8d8b92563
dim4o <dimcho.nedev+git@gmail.com>
2020-08-28 14:24:20 +0300

    Update Decorator pattern example

--- src/kb/design_patterns/decorator/JavaIOApp.java
+++ src/kb/design_patterns/decorator/JavaIOApp.java
@@ -8,20 +8,24 @@ import kb.design_patterns.decorator.io.CommaInputStream;
 import kb.design_patterns.decorator.io.LowerCaseInputStream;
 
 public class JavaIOApp {
+    
 
     public static void main(String[] args) throws IOException {
         System.out.println("USER DIR: " + System.getProperty("user.dir") + "\n");
+        final String filePath = "./src/kb/design_patterns/decorator/test.txt";
+        
         int c = 0;
-        try (InputStream in = new LowerCaseInputStream(
-                new CommaInputStream(new FileInputStream("./test.txt")))) {
+        try (InputStream in = new LowerCaseInputStream(new CommaInputStream(
+                new FileInputStream(filePath)))) {
             while ((c = in.read()) >= 0)
                 System.out.print((char) c);
         }
-        
+
         System.out.println();
-        
+
         c = 0;
-        try (InputStream in = new LowerCaseInputStream(new FileInputStream("./test.txt"))) {
+        try (InputStream in = new LowerCaseInputStream(
+                new FileInputStream(filePath))) {
             while ((c = in.read()) >= 0)
                 System.out.print((char) c);
         }
